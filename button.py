9dbc29e31eac7e502942193bec9adf6a0fb9c2a6
xxMichalPK <michal10.05.pk@gmail.com>
2023-12-01 14:55:57 +0100

    Add button to main menu

---
+++ button.py
@@ -0,0 +1,34 @@
+import pygame, os
+from config import *
+
+#button class
+class Button():
+	def __init__(self, x, y, image, text, scale):
+		width = image.get_width()
+		height = image.get_height()
+		self.font = pygame.font.Font(os.path.join("resources", "fonts", "PixeloidSans-Bold.ttf"), 40)
+		self.fontImg = self.font.render(text, True, (255, 255, 255))
+		self.image = pygame.transform.scale(image, (int(width * scale), int(height * scale)))
+		self.rect = self.image.get_rect()
+		self.rect.topleft = (x, y)
+		self.clicked = False
+
+	def draw(self, screen):
+		action = False
+		#get mouse position
+		pos = pygame.mouse.get_pos()
+
+		#check mouseover and clicked conditions
+		if self.rect.collidepoint(pos):
+			if pygame.mouse.get_pressed()[0] == 1 and self.clicked == False:
+				self.clicked = True
+				action = True
+
+		if pygame.mouse.get_pressed()[0] == 0:
+			self.clicked = False
+
+		#draw button on screen
+		screen.blit(self.image, (self.rect.x, self.rect.y))
+		screen.blit(self.fontImg, ((self.rect.x + self.rect.width / 2) - (self.fontImg.get_width() / 2), (self.rect.y + self.rect.height / 2) - (self.fontImg.get_height() / 2)))
+
+		return action
--- mainMenu.py
+++ mainMenu.py
@@ -1,11 +1,16 @@
 import pygame, os
 from config import *
+from button import *
 
 class MainMenu:
     def __init__(self):
         self.font = pygame.font.Font(os.path.join("resources", "fonts", "PixeloidSans-Bold.ttf"), 40)
         self.fontImg = self.font.render(GAME_TITLE, True, (255, 255, 255))
+        self.startBtnImg = pygame.image.load("resources/startBtn.png").convert_alpha()
+        self.startBtn = Button((WINDOW_WIDTH / 2) - (self.startBtnImg.get_width() / 2), 70, self.startBtnImg, "Start", 1)
         
     def draw(self, screen):
-        screen.fill((50, 50, 60))
+        screen.fill((30, 30, 30))
         screen.blit(self.fontImg, ((WINDOW_WIDTH / 2) - (self.fontImg.get_width() / 2), 20))
+        if self.startBtn.draw(screen):
+            exit(0)
