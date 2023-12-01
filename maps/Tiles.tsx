9e59b274b6c9b008cef313072ef47d92f102f732
jeli-t <jelitotomek@gmail.com>
2023-12-01 18:33:42 +0100

    fix map loading issues

--- maps/Tiles.tsx
+++ maps/Tiles.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.2" name="Tiles" tilewidth="32" tileheight="32" tilecount="70" columns="10">
- <image source="../resources/tileset.png" width="320" height="224"/>
+<tileset version="1.9" tiledversion="1.9.2" name="tiles" tilewidth="32" tileheight="32" tilecount="1980" columns="60">
+ <image source="tiles.png" width="1920" height="1080"/>
 </tileset>
