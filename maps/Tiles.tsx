9e10eb71a320d683a24c5ab8de041ae3cc12a5aa
xxMichalPK <michal10.05.pk@gmail.com>
2023-12-01 15:02:35 +0100

    Fix tileset path

--- maps/Tiles.tsx
+++ maps/Tiles.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.10.2" name="Tiles" tilewidth="32" tileheight="32" tilecount="70" columns="10">
- <image source="../../../Personal/High School/KL 5pi/ADM/DungeonGen/resources/tileset.png" width="320" height="224"/>
+ <image source="../resources/tileset.png" width="320" height="224"/>
 </tileset>
